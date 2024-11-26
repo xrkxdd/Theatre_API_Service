b4ba75b6f932ff7d444004c723f8b344c005fa1a
turb0 <nazar.mc28@gmail.com>
2024-11-26 16:41:10 +0100

    final edits

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,22 @@
-FROM Python:3.12.3
+FROM python:3.11-alpine3.20
+
 LABEL maintainer="nazar.mc28@gmail.com"
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+RUN mkdir -p /files/media
+
+RUN adduser \
+    --disabled-password \
+    --no-create-home \
+    my_user
+
+RUN chown -R my_user /files/media
+RUN chmod -R 755 /files/media
+
+USER my_user
--- Theatre_API_Service/settings.py
+++ Theatre_API_Service/settings.py
@@ -85,13 +85,17 @@ WSGI_APPLICATION = 'Theatre_API_Service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
